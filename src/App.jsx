e7acc281ffcf25f3f4b8bc69c9a24ad3fe2a3cbe
OLADIMEJI988 <oladimejimax49@gmail.com>
2025-09-23 08:56:51 +0100

    Initial boilerplate app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-5xl'>
+        hey
+      </div>
+    </>
+  )
+}
+
+export default App

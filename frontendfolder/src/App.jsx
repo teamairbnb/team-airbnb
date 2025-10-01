ee162a7188e8cb6b57dabd6127d0725c63bc106f
Mel <melodynk01@gmail.com>
2025-10-01 10:58:56 +0100

    feat: update login page styling and validation

--- frontendfolder/src/App.jsx
+++ frontendfolder/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import './index.css'
+import Login from './assets/components/Login'
 
 function App() {
 
   return (
     <>
-      <div className='text-5xl'>Hello from the frontend</div>
+      <Login />
     </>
   )
 }
---
+++ frontendfolder/src/assets/components/Logo.jsx
@@ -0,0 +1,18 @@
+import mainlogo from "../images/mainlogo.png";
+import logo from "../images/logo.png";
+function Logo() {
+  return (
+    <div className="flex flex-col p-[25px] -translate-y-10 w-full rounded-[50px] h-[290px] bg-cover bg-center relative" style={{backgroundImage: `url(${mainlogo})`}}>
+
+        <div className="relative flex flex-col items-center justify-end h-full mb-4">
+            <img src={logo} className="w-[65px]" alt="App logo" />
+            <div className="text-white tracking-wide">
+<p className="font-semibold text-[33px] mt-[8px]">RENCAR</p>
+<p className="text-[14px] text-center">BUSINESS</p>
+            </div>
+        </div>
+      
+    </div>
+  );
+}
+export default Logo;

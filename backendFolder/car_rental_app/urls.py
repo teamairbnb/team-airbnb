3a0cbd9b2b641eb26e0e3cb79d5a6c1c97296751
abasiofon-sendan <abasiofon135@gmail.com>
2025-10-02 12:19:16 +0100

    i versioned the api endpoint

--- backendFolder/car_rental_app/urls.py
+++ backendFolder/car_rental_app/urls.py
@@ -19,7 +19,15 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
-    path('api/', include('accounts.urls'))
+    path('api/v1/', include([
+        path('auth/', include('djoser.urls')),
+        path('auth/', include('djoser.urls.jwt')),
+        path('app/', include('accounts.urls'))
+
+    ]
+
+    ),
+    
+    )
+
 ]

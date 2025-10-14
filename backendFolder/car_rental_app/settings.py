0107ca7eae70c60e003e54e9adbed4ba43627e63
abasiofon-sendan <abasiofon135@gmail.com>
2025-10-14 17:43:15 +0100

    email config added

--- backendFolder/car_rental_app/settings.py
+++ backendFolder/car_rental_app/settings.py
@@ -158,8 +158,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email configuration
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
